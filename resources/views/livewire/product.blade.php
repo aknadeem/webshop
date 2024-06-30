3ad12de514f45050d9192e5e28c21ef8863eab56
Nadeem Ahmed <aknadeemahmed@gmail.com>
2024-06-30 19:10:53 +0500

    dynamic image selector using apline

---
+++ resources/views/livewire/product.blade.php
@@ -0,0 +1,34 @@
+<div class="grid grid-cols-2 gap-10 my-12">
+    <div class="space-y-4" x-data="{ image: '/{{$this->product->image?->path}}' }">
+        <div class="bg-white p-5 rounded-lg shadow">
+            <img x-bind:src="image" alt="no image" />
+        </div>
+
+        <div class="grid grid-cols-4 gap-10">
+            @foreach($this->product->productImages as $image)
+                <div class="bg-white p-5 rounded shadow">
+                    <img src="/{{$image->path}}" @click="image = '/{{$image->path}}'" class="rounded cursor-pointer" alt="no image" />
+                </div>
+            @endforeach
+        </div>
+    </div>
+    <div>
+        <h1 class="text-3xl font-medium"> {{$this->product?->name}} </h1>
+        <div class="text-xl text-gray-700"> {{$this->product?->price}} </div>
+
+        <div class="mt-4">
+            {{ $this->product?->description }}
+        </div>
+        
+        <div class="mt-4 space-y-4">
+            <select class="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-800">
+                @foreach($this->product->variants as $variant)
+                    <option value="{{$variant->id}}"> {{$variant->size}} / {{$variant->color}} </option>
+
+                @endforeach
+            </select>
+            <x-button>Add to Cart</x-button>
+        </div>
+        
+    </div>
+</div>
